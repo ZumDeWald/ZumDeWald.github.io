1fac1456b04e1b5e862673dc257fc3470f8b0bb9
ZumDeWald <zumdewald@gmail.com>
2018-05-26 13:09:58 -0500

    REFACTOR
    Refactored all code
    HTML - changed cards to list items and removed unnecessary tags
    CSS - removed unnecessary code for removed HTML tags
    JS - Reset, will need rebuilt from scratch under new direction

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,13 @@ $(document).ready(() => {
 
 /* VARIABLES */
 
-const card = $(".card, .card>*");
 
 
 /* FUNCTIONS */
 
-const showCard = (target) => {
-  target.children().toggleClass("card-hide");
-};
+
 
 /* Flip Card on Click */
-
- card.click((e) => {
-   showCard($(e.target));
-});
-
 
 
 });
