6d3ea1395a122034c68069c78859198e7104dc05
ZumDeWald <zumdewald@gmail.com>
2018-05-26 13:17:48 -0500

    Cards now clickable
    CSS tags added to show / hide cards
    JS function to change between show / hide

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
 $(document).ready(() => {
 
 /* VARIABLES */
-
+const card = $("li");
 
 
 /* FUNCTIONS */
 
-
-
 /* Flip Card on Click */
-
+card.click((e) => {
+  $(e.target).toggleClass("card-show");
+  $(e.target).toggleClass("card-hide");
+})
 
 });
