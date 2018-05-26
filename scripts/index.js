674c4212e8edb9fe20e2ea0bb41daa76b7e822cb
ZumDeWald <zumdewald@gmail.com>
2018-05-26 14:14:16 -0500

    Card Flip working
    Built logic to flip cards and keep shown if they match
    Cards will flip back over if they do not match
    Needs transitional animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,40 @@
 $(document).ready(() => {
 
 /* VARIABLES */
-const card = $("li");
+const card = $(".card-hide");
 
 
 /* FUNCTIONS */
 
 /* Flip Card on Click */
-card.click((e) => {
-  $(e.target).toggleClass("card-show");
-  $(e.target).toggleClass("card-hide");
-})
+const cardFlip = (target) => {
+  target.toggleClass("card-show");
+  target.toggleClass("card-hide");
+}
+
+/* Check if card matches */
+let cardShowing = false;
+let cardValue = "";
+
+card.click((e) =>{
+  cardFlip($(e.target));
+  if (!cardShowing) {
+    $(e.target).addClass("marker");
+    cardValue = $(e.target).children().attr("class");
+    cardShowing = true;
+  } else {
+    if (cardValue == $(e.target).children().attr("class")) {
+//      alert("You Got A Match!");
+      card.removeClass("marker");
+    } else {
+      $(e.target).addClass("marker");
+//      alert("Sorry, not a match");
+      cardFlip($(".marker"));
+      card.removeClass("marker");
+    }
+    cardShowing = false;
+  }
+ });
+
 
 });
