250f9f0739bbc38a077f81114a5d0ed167d82484
ZumDeWald <zumdewald@gmail.com>
2018-07-17 23:14:16 -0500

    UPDATE with current code

--- games/SillyStories/scripts/index.js
+++ games/SillyStories/scripts/index.js
@@ -216,7 +216,7 @@ const switcher = () => {
 
     if (tracker) {
 
-    $storyText.text(`Hello, my name is ${$propName} ! I have a ${$noun1} , ${$num1} ${$noun2} s, and a ${$noun3} . My biggest focus lately has been my ${$noun4} . ${$verb1} over ${$num2} hours a day has kept me ${$adj1} . Hopefully my ${$adj2} skills in ${$skill1} will get me a new ${$noun5} . My ${$fam1} thinks I am ${$feel1} and should be ${$verb2} instead.`);
+    $storyText.text(`Hello, my name is ${$propName} ! I have a ${$noun1} , ${$num1} ${$noun2} (s), and a ${$noun3} . My biggest focus lately has been my ${$noun4} . ${$verb1} (ing) over ${$num2} hours a day has kept me ${$adj1} . Hopefully my ${$adj2} skills in ${$skill1} will get me a new ${$noun5} . My ${$fam1} thinks I am ${$feel1} and should ${$verb2} instead.`);
 
   } else {
     $storyText.text(`My favorite game has ${$num1} ${$noun1} (s), ${$num2} ${$noun2} (s), and a very ${$adj1} ${$noun3} ! You can play it with your ${$fam1} or a friend. Don\'t ${$verb1} too close to the ${$adj2} ${$noun1} or you\'re ${$adv1} out!`);
