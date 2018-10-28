ac6a3df9ea6b0e745044ede4c1a32a6c21d3aa1e
ZumDeWald <zumdewald@gmail.com>
2018-10-27 22:44:20 -0500

    Trying some things

---
+++ build/precache-manifest.951af31ccea1f1136365db0374a51a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "988a565eded05027eb16",
+    "url": "/static/js/main.988a565e.chunk.js"
+  },
+  {
+    "revision": "dafcf796f3b29a71eb10",
+    "url": "/static/js/1.dafcf796.chunk.js"
+  },
+  {
+    "revision": "988a565eded05027eb16",
+    "url": "/static/css/main.0806da97.chunk.css"
+  },
+  {
+    "revision": "666784b824a062119f31263c4f4a6bb1",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89c9649ba437017f12343bc1dd5536dc.js"
+  "/precache-manifest.951af31ccea1f1136365db0374a51a58.js"
 );
 
 workbox.clientsClaim();
