ca0727d475185387905715ca05ceecc2aa48bde0
ZumDeWald <zumdewald@gmail.com>
2018-11-20 22:50:07 -0600

    Updates

---
+++ precache-manifest.c6ea65b64575301e9968331b1db45582.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7b0b64e882caa13cb92307eb432cd88",
+    "url": "/static/media/STP7.a7b0b64e.jpg"
+  },
+  {
+    "revision": "8f8cd6ea0b35fc23a4167e0a7919b5f1",
+    "url": "/static/media/STP6.8f8cd6ea.jpg"
+  },
+  {
+    "revision": "d9614cc8d0581d4c32524bf2c39812b5",
+    "url": "/static/media/STP5.d9614cc8.jpg"
+  },
+  {
+    "revision": "41701ad1b9f50710ed0970210878e8cf",
+    "url": "/static/media/STP2.41701ad1.jpg"
+  },
+  {
+    "revision": "886cf74d1c2e614f030fdba7d25d0cba",
+    "url": "/static/media/STP1.886cf74d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70455c821c839e29e7ac",
+    "url": "/static/js/main.70455c82.chunk.js"
+  },
+  {
+    "revision": "e458ab7034dd113fa862",
+    "url": "/static/js/1.e458ab70.chunk.js"
+  },
+  {
+    "revision": "70455c821c839e29e7ac",
+    "url": "/static/css/main.1a075115.chunk.css"
+  },
+  {
+    "revision": "d112355cc2e103dabd06286ad52a1250",
+    "url": "/index.html"
+  }
+];
