5832b9185c6aaf088210c432ef0ffa4f58cf2dfa
ZumDeWald <zumdewald@gmail.com>
2018-12-16 22:29:43 -0600

    Push Updates 12162018 10:30PM

--- src/About.js
+++ src/About.js
@@ -17,109 +17,114 @@ class About extends Component {
     return (
       <section id="about-content-container" className="dark-bg">
       <ScrollToTopOnMount />
+      <h1 className="about-faq">FAQ</h1>
       <article className="about-article-area">
-        <h1 className="about-faq">FAQ</h1>
         <div className="about-article-box box-closed">
           <div className="about-box-title-box">
             <i className="far fa-question-circle fa-3x about-box-icon"></i>
             <h2 className="about-box-heading main-font-style">What should I expect from my session?</h2>
           </div>
+          <h3 className="about-box-sub-header">Each Session</h3>
           <p className="about-box-text">
             My goal on every shoot is to capture true expressions. The sweet little smiles, the tears, the quirky expressions and even the somber faces <em>which are my favorite!</em> </p>
           <p className="about-box-text">
-            I shoot both on location and have an in home studio. I do not do family sessions at my in home studio. </p>
+            I will direct some posing, but my goal is to capture <em>personality</em> and <em>natural</em> body language.</p>
           <p className="about-box-text">
-            I will direct some posing, but my goal is to capture personality and natural body language.</p>
-            <p className="about-box-text">
-              I shoot on location either 1-2 hours before sunset or early morning around 8am or 9am depending on the season.</p>
-
-        <h3 className="about-box-sub-header">Newborn Specific</h3>
-
+            I shoot both on location and have an in home studio. <em>(I <strong>do not do</strong> family sessions at my in home studio)</em> </p>
           <p className="about-box-text">
-            For my Newborn Session clients, you will receive step by step instructions on how to prepare your baby for their session.  You will also receive a questionaire so I can better prepare for your unique stylized session. </p>
+            An on-location shoot works best either 1-2 hours before sunset or early morning around 8am or 9am depending on the season.</p>
+          <h3 className="about-box-sub-header">Newborn Specific</h3>
+          <p className="about-box-text">
+            For my Newborn Session clients, you will receive step by step instructions on how to prepare your baby for their session.  You will also receive a questionaire so I can better prepare for your <em>unique, stylized</em> session. </p>
           <p className="about-box-text">
             I have absolutely everything needed for a beautifully styled newborn session!  All you have to bring is your new, sweet baby, extra formula if you are bottle feeding, a pacifier, and snacks for yourselves! </p>
           <p className="about-box-text">
             Please come being prepared to RELAX!  I have Netflix and WiFi. You are welcome to watch the session, but it is not necessary. I actually encourage my parents to sit back and relax. Your baby is in good hands! And what a sweet surprise it will be when you receive your edited images! </p>
           <p className="about-box-text">
-            An informational sheet will be e-mailed to you, so you know how to best prepare your newborn for a great session.  Yes, there are actually things you can do they day of the session to help prepare your little one to be calm, sleepy, and ready! </p>
-          <p className="about-box-text">
-            I will also send you a questionnaire so I can better prepare for your little ones uniquely styled session. </p>
-          <p className="about-box-text">
           <i className="fas fa-asterisk fa-sm"></i> &nbsp;
           <em>Plan on the session length being at least 2 hours.</em></p>
-            <span
-              onClick={this.toggleAboutBox}
-              className="about-box-expand">
-              <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
-            </span>
-          </div>
-
-          <div className="about-article-box box-closed">
-            <div className="about-box-title-box">
-              <i className="far fa-question-circle fa-3x about-box-icon"></i>
-              <h2 className="about-box-heading main-font-style">How should I prepare for my session?</h2>
-            </div>
-
-            <p className="about-box-text">
-              Please coordinate! Clothing can really make or break a photo shoot, and that is why I can assist you in pulling your wardrobe together. I will absolutely stay true to your style while keeping in mind what looks good on camera. I am creating art, and clothing is a tool to make that art come to life.</p>
-            <p className="about-box-text">
-              During the session, expect to have fun and be yourself. I might ask you to do silly or fun things to help break the ice or to create connections with you.</p>
-            <p className="about-box-text">
-              Please come being prepared to RELAX!  I have Netflix and WiFi. You are welcome to watch the session, but it is not necessary. I actually encourage my parents to sit back and relax. Your baby is in good hands! And what a sweet surprise it will be when you receive your edited images! </p>
-              <span
-                onClick={this.toggleAboutBox}
-                className="about-box-expand">
-                <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
-              </span>
-            </div>
-
-            <div className="about-article-box box-closed">
-              <div className="about-box-title-box">
-                <i className="far fa-question-circle fa-3x about-box-icon"></i>
-                <h2 className="about-box-heading main-font-style">What is included in a session?</h2>
-              </div>
-
-              <h3 className="about-box-sub-header">Included for each session:</h3>
-
-              <ul>
-                <li>Photographer’s time and talent</li>
-                <li>Professionally edited images</li>
-                <li>File preparation, and an online gallery of images for you to easily share with family & friends as well as downloading and printing. [ <em>You will have one month for viewing from the time the gallery is available.</em> ]</li>
-                <li>All photos come with print release & lab recommendations.</li>
-                <li>All prints are purchased separately. </li>
-                <li>You can expect your images to be edited between 1-2 weeks.</li>
-                <li>If you prefer a flash drive with your images one can be purchased for an extra fee.</li>
-                <li>I often share a sneak peak for the family on Facebook within 48 hours of the session. <strong>When posting pictures online, I may share baby’s first name and small details like weight and age.</strong> You are welcome to tag and share these photos after 24 hours of me posting them if you would like to.  </li>
-              </ul>
-              <span
-                onClick={this.toggleAboutBox}
-                className="about-box-expand">
-                <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
-              </span>
-            </div>
-
-            <div className="about-article-box box-closed">
-              <div className="about-box-title-box">
-                <i className="far fa-question-circle fa-3x about-box-icon"></i>
-                <h2 className="about-box-heading main-font-style">How are payments handled?</h2>
-              </div>
-
-              <p className="about-box-text">
-                The remainder of the session fee minus the deposit is due at the time of the session by either cash or pay-pal. </p>
-              <p className="about-box-text">
-                During the session, expect to have fun and be yourself. I might ask you to do silly or fun things to help break the ice or to create connections with you.</p>
-              <p className="about-box-text">
-                Please come being prepared to RELAX!  I have Netflix and WiFi. You are welcome to watch the session, but it is not necessary. I actually encourage my parents to sit back and relax. Your baby is in good hands! And what a sweet surprise it will be when you receive your edited images! </p>
-
           <span
             onClick={this.toggleAboutBox}
             className="about-box-expand">
             <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
           </span>
         </div>
-
       </article>
+
+      <article className="about-article-area">
+        <div className="about-article-box box-closed">
+          <div className="about-box-title-box">
+            <i className="far fa-question-circle fa-3x about-box-icon"></i>
+            <h2 className="about-box-heading main-font-style">How should I prepare for my session?</h2>
+          </div>
+          <p className="about-box-text">
+            <strong>Clothing</strong> can really make or break a photo shoot, and that is why I can assist you in pulling your wardrobe together. Please coordinate! I will absolutely stay true to <em>your style</em> while keeping in mind what looks good on camera. I am creating art, and <em>clothing</em> is a tool to make that art come to life.</p>
+          <p className="about-box-text">
+            During the session, expect to <strong>have fun and be yourself</strong>. I might ask you to do silly or fun things to help break the ice or to create connections with you.</p>
+          <span
+            onClick={this.toggleAboutBox}
+            className="about-box-expand">
+            <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
+          </span>
+        </div>
+      </article>
+
+      <article className="about-article-area">
+        <div className="about-article-box box-closed">
+          <div className="about-box-title-box">
+            <i className="far fa-question-circle fa-3x about-box-icon"></i>
+            <h2 className="about-box-heading main-font-style">What is included in a session?</h2>
+          </div>
+          <h3 className="about-box-sub-header">Included for each session:</h3>
+          <ul className="about-list">
+            <li className="about-list-item">Photographer’s time and talent</li>
+            <li className="about-list-item">Professionally edited images [ <em>quantity specified per package on the Pricing page</em> ]</li>
+            <li className="about-list-item">Additional images can be purchased for $20 per image</li>
+            <li className="about-list-item">File preparation and an online gallery of images for you to easily share with family & friends, download, and print. <br />
+            &nbsp; - You will have <strong>one month</strong> for viewing from the time the gallery is available.</li>
+            <li className="about-list-item">All photos come with print release & lab recommendations.</li>
+            <li className="about-list-item">All prints are purchased separately. </li>
+            <li className="about-list-item">You can expect your images to be edited between 1-2 weeks.</li>
+            <li className="about-list-item">If you prefer a flash drive with your images one can be purchased for an extra fee.</li>
+            <li className="about-list-item">I often share a sneak peak for the family on Facebook within 48 hours of the session. <br /> &nbsp; - <strong>When posting pictures online, I may share baby’s first name and small details like weight and age.</strong> <br />
+            &nbsp; - You are welcome to tag and share these photos after 24 hours of me posting them if you would like to.  </li>
+          </ul>
+          <span
+            onClick={this.toggleAboutBox}
+            className="about-box-expand">
+            <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
+          </span>
+        </div>
+      </article>
+
+      <article className="about-article-area">
+        <div className="about-article-box box-closed">
+          <div className="about-box-title-box">
+            <i className="far fa-question-circle fa-3x about-box-icon"></i>
+            <h2 className="about-box-heading main-font-style">Payment / Fee information:</h2>
+          </div>
+          <p className="about-box-text">
+            I accept Cash or Pay-Pal </p>
+          <p className="about-box-text">
+            A deposit [ <em>50% of the session cost</em> ] is due at booking </p>
+          <p className="about-box-text">
+            The remainder of the session fee [ <em>minus the deposit</em> ] is due at the time of the session </p>
+          <p className="about-box-text">
+            Session are <em>only</em> booked for weekdays. Should you need a weekend session there is an additional $50 fee</p>
+          <p className="about-box-text">
+            Travel fee applies on distances greater than 25 miles [ <em>fee changes dependent on mileage beyond 25</em> ]</p>
+          <p className="about-box-text">
+            All payments are non-refundable</p>
+          <p className="about-box-text">
+            Pricing secured upon booking your session. Pricing is non-negotiable</p>
+          <span
+            onClick={this.toggleAboutBox}
+            className="about-box-expand">
+            <i className="fas fa-angle-down fa-2x prevent-click about-box-icon"></i>
+          </span>
+        </div>
+      </article>
+
       </section>
     );
   }
